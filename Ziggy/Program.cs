cb0f2d342ab1b408ead5fe6b353eeb9c7ad804ad
Connor Sullivan <cksullvn@gmail.com>
2020-03-31 16:39:33 -0500

    created initial setup file with gitignore

---
+++ Ziggy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ziggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
